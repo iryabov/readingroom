a7f9a898233e43aff84129304c632cd3678afe66
iryabov <iryabov@i-novus.ru>
2021-03-13 15:32:44 +0300

    Init commit

---
+++ src/main/kotlin/com/github/iryabov/readingroom/ReadingroomApplication.kt
@@ -0,0 +1,11 @@
+package com.github.iryabov.readingroom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReadingroomApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReadingroomApplication>(*args)
+}
