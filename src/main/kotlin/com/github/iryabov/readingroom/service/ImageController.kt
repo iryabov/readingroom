5e733e7416fe339bfdf67f23b07a00d173825ebc
iryabov <iryabov@i-novus.ru>
2021-03-20 11:50:33 +0300

    Added images converter to base64

---
+++ src/main/kotlin/com/github/iryabov/readingroom/service/ImageController.kt
@@ -0,0 +1,27 @@
+package com.github.iryabov.readingroom.service
+
+import org.apache.commons.io.IOUtils
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
+
+@RestController
+@RequestMapping("/images")
+class ImageController {
+    @CrossOrigin(origins = ["*"])
+    @PostMapping("/convert")
+    fun convert(@RequestParam("file") file: MultipartFile): ImageInfo {
+        val content = IOUtils.toByteArray(file.inputStream)
+        val encodedString: String = Base64.getEncoder().encodeToString(content)
+        return ImageInfo(
+                url = "data:image/png;base64,$encodedString",
+                id = file.originalFilename
+        )
+    }
+}
+
+class ImageInfo(
+        val url: String,
+        val id: String? = null
+)
