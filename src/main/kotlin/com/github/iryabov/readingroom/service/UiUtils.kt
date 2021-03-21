d90797e3739ee536cca54a5299bfecf18fccc8a3
iryabov <iryabov@i-novus.ru>
2021-03-21 10:12:58 +0300

    Added editing book

--- src/main/kotlin/com/github/iryabov/readingroom/service/UiUtils.kt
+++ src/main/kotlin/com/github/iryabov/readingroom/service/UiUtils.kt
@@ -1,10 +1,14 @@
 package com.github.iryabov.readingroom.service
 
-fun short(text: String?, length: Int): String {
+fun shorten(text: String?, length: Int): String {
     if (text == null)
         return ""
     return if (text.length < length)
         text
     else
         "${text.substring(0, length - 1)}..."
+}
+
+fun split(text: String?, separator: String = ", "): List<String> {
+    return if (text.isNullOrEmpty()) return emptyList() else text.split(separator)
 }
