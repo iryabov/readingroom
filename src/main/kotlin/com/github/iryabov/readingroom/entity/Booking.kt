1b9e0031f4525a5324aa302f2cb5e0ad8a9cc959
iryabov <iryabov@i-novus.ru>
2021-04-03 08:02:32 +0300

    Added booking

--- src/main/kotlin/com/github/iryabov/readingroom/entity/Booking.kt
+++ src/main/kotlin/com/github/iryabov/readingroom/entity/Booking.kt
@@ -10,9 +10,11 @@ class Booking(
         var book: Book,
         @ManyToOne
         var member: Member,
-        var bookingFrom: LocalDate,
-        var bookingUntil: LocalDate,
+        var bookingFrom: LocalDate? = null,
+        var bookingUntil: LocalDate? = null,
         var createdAt: LocalDate = LocalDate.now(),
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Int? = null
-)
+) {
+        constructor(): this(Book(), Member())
+}
--- src/main/kotlin/com/github/iryabov/readingroom/model/BookModel.kt
+++
@@ -1,7 +0,0 @@
-package com.github.iryabov.readingroom.model
-
-class BookModel(
-        val name: String,
-        val authors: List<String>,
-        val description: String
-)
